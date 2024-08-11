b6e8c1fad5db188be946dcff0edf73c18fb90669
Vladimir <vova2066@yandex.ru>
2024-08-11 11:06:26 +0300

    ++

--- src/scripts/blockScrollBody.js
+++ src/scripts/blockScrollBody.js
@@ -14,11 +14,3 @@ function unblockScrollBody () {
     html.classList.remove(classBlockScroll);
   }
 };
-
-function toggleBlockScrollBody () {
-  if ( html.classList.contains(classBlockScroll) ) {
-    html.classList.remove(classBlockScroll);
-  } else {
-    html.classList.add(classBlockScroll);
-  }
-};
